e96a3d5e3e345736b863c16052ad719f0241e5a8
hlino77 <ghkddlsqja77@naver.com>
2023-10-04 14:03:23 +0900

    Fix : 이동동기화 수정

--- MainFrameWork/Client/Private/State_Naruto_RunLoop.cpp
+++ MainFrameWork/Client/Private/State_Naruto_RunLoop.cpp
@@ -127,7 +127,7 @@ void CState_Naruto_RunLoop::Set_TargetPos(Vec3 vDir)
 {
 	Vec3 vPos = m_pPlayer->Get_TransformCom()->Get_State(CTransform::STATE::STATE_POSITION);
 
-	Vec3 vTargetPos = vPos + vDir * 0.1f;
+	Vec3 vTargetPos = vPos + vDir * 2.0f;
 	m_pPlayer->Set_TargetPos(vTargetPos);
 }
 
